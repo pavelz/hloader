82876ef10d711bc3027243adc6ca4d0e89aeca4c
Pavel Zaitsev <pavel@arslogi.ca>
2024-12-09 10:43:12 -0500

    shamlet lol:wq

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,23 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main where
 
 import Socket
+import Text.Hamlet (shamlet)
+import Text.Blaze.Html.Renderer.String (renderHtml)
+
+
+-- what is the hloader
+-- load url into files and serve them to the visitor
+-- form to get url from the user
+-- backend to load file into memory
+-- backend to send urlencoded upload to the user
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
   print $ Socket.socket
+  let htmlfoo = [shamlet|
+        dasdassadsad
+        |] 
+  putStrLn $ renderHtml htmlfoo
